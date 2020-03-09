1a03f50297957ac28aaad8667c864c4c2031cc9f
Aaron Caswell <acaswell@itbit.com>
2020-03-09 08:44:34 -0700

    WIP

--- src/app.ts
+++ src/app.ts
@@ -19,19 +19,25 @@ const eos = new Api({
   textEncoder,
 })
 
-const producer_account:string = config.get('producer_account')
+const producer_accounts:any = config.get('producer_accounts')
 const producer_permission:string = config.get('producer_permission')
 const producer_website:string = config.get('producer_website')
 const producer_location:number = config.get('producer_location')
-let producer_signing_pubkeys:any = config.get('producer_signing_pubkeys')
+let producer_signing_pubkeys_nodes:any = config.get('producer_signing_pubkeys_nodes')
 
 const rounds_missed_threshold:number = config.has('rounds_missed_threshold') ? config.get('rounds_missed_threshold') : 1
 const total_producers:number = config.has('total_producers') ? config.get('total_producers') : 21
 const round_timer:number = (config.has('round_timer') ? Number(config.get('round_timer')) : 126) * 1000
 
 let stuck_counter = 0
-let rounds_missed = 0
-let last_unpaid = -1
+
+const producersTracking:any = _.map(producer_accounts, (producer_account) => {
+  return {
+    rounds_missed: 0,
+    last_unpaid: -1,
+    producer_account
+  }
+})
 
 function throttle(num: number): boolean {
   const sqrt = Math.floor(Math.pow(num, 0.5))
@@ -84,12 +90,13 @@ export async function getProducerSchedule(): Promise<any> {
 }
 
 export function incomingSigningKeyChange(
+  producerTracking: any,
   current_signing_key: string,
   schedule: any,
   state: string
 ): boolean {
   if (schedule[state] && schedule[state].producers) {
-    const schedule_for_producer = _.find(schedule[state].producers, { producer_name: producer_account })
+    const schedule_for_producer = _.find(schedule[state].producers, { producer_name: producerTracking.producer_account })
     if (schedule_for_producer) {
       const scheduled_new_key = schedule_for_producer.block_signing_key
       if (producer_signing_pubkeys.includes(scheduled_new_key)) {
@@ -105,7 +112,7 @@ export function incomingSigningKeyChange(
   return false
 }
 
-export async function failover(next_signing_key: string): Promise<string> {
+export async function failover(producer_account: string, next_signing_key: string): Promise<string> {
   const result = await eos.transact({
     actions: [
       {
@@ -152,6 +159,75 @@ export async function unregister(): Promise<string> {
   return result.transaction_id
 }
 
+async function checkProducer(producerTracking: any, state: any, producers: any, schedule: any) {
+  // Get the target producer from the active producers
+  const producer = _.find(producers, { owner: producerTracking.producer_account })
+  if (!producer) {
+    logger.debug({ producerTracking.producer_account }, `producer not an active producer`)
+    return
+  }
+
+  // Get the target producer within the schedule
+  const current_schedule = _.find(schedule.active.producers, { producer_name: producerTracking.producer_account })
+  if (!current_schedule) {
+    logger.debug({ producerTracking.producer_account }, `producer not in schedule`)
+    return
+  }
+
+  // Get the current signing key from the active schedule
+  const current_signing_key = current_schedule.block_signing_key;
+
+  // Ensure the current key in use is not a potential key to failover to
+  if (_.any(producer_signing_pubkeys_nodes, producer_signing_pubkeys => producer_signing_pubkeys.includes(current_signing_key))) {
+    logger.debug({ producer_signing_pubkeys_nodes, current_signing_key }, "removing current signing key set from potential failover keys")
+    producer_signing_pubkeys_nodes = _.filter(producer_signing_pubkeys_nodes, (producer_signing_pubkeys) => !producer_signing_pubkeys.includes(current_signing_key));
+  }
+
+  // If a key change is in progress, do not proceed
+  if (
+    incomingSigningKeyChange(producerTracking, current_signing_key, schedule, 'proposed')
+    || incomingSigningKeyChange(producerTracking, current_signing_key, schedule, 'pending')
+  ) {
+    // reset the last paid value to reinitialize and prevent secondary trigger under certain circumstances
+    last_unpaid = -1
+    slack.send(`🕒 signing key changes pending in schedule`)
+    return
+  }
+
+  let { unpaid_blocks } = producer
+  logger.debug({ unpaid_blocks, last_unpaid: producerTracking.last_unpaid }, "unpaid block states")
+
+  if (producerTracking.last_unpaid === -1) {
+    // If this is the first run (-1), just initialize and don't proceed
+    logger.info({ unpaid_blocks }, "initializing unpaid block count on first call")
+    producerTracking.last_unpaid = unpaid_blocks
+    return
+  } else if (unpaid_blocks < producerTracking.last_unpaid) {
+    // If the new unpaid is less than the old, the BP has claimed and this value needs to be reset
+    logger.debug({ last_unpaid: producerTracking.last_unpaid, unpaid_blocks }, "resetting unpaid blocks, encountered reward claim")
+    producerTracking.last_unpaid = unpaid_blocks
+    return
+  } else if (unpaid_blocks > producerTracking.last_unpaid) {
+    // If the unpaid count is higher than the old count, new blocks were produced
+    const newBlocks = unpaid_blocks - producerTracking.last_unpaid
+    logger.info({ last_unpaid: producerTracking.last_unpaid, unpaid_blocks }, `round success, witnessed ${newBlocks} new unpaid blocks`)
+    // Update the last unpaid value
+    producerTracking.last_unpaid = unpaid_blocks
+    // If any previously recorded rounds were recorded, reset them
+    if (producerTracking.rounds_missed > 0) {
+      const msg = `producer recovered after ${producerTracking.rounds_missed} rounds, witnessed ${newBlocks} new unpaid blocks`
+      logger.info({ rounds_missed: producerTracking.rounds_missed }, msg)
+      slack.send(msg)
+      producerTracking.rounds_missed = 0
+    }
+  } else if (unpaid_blocks === producerTracking.last_unpaid) {
+    // If the unpaid blocks is the same as the last time - no new blocks have been produced
+    producerTracking.rounds_missed += 1
+    logger.info({ last_unpaid: producerTracking.last_unpaid, unpaid_blocks, rounds_missed: producerTracking.rounds_missed }, `producer missed a round!`)
+    slack.send(`⚠️ producer missed a round ${producerTracking.rounds_missed}/${rounds_missed_threshold}`)
+  }
+}
+
 export async function check() {
   // Retrieve the current state of the blockchain
   const state = await getChainState()
@@ -164,89 +240,30 @@ export async function check() {
   // Get the current producer schedule
   const schedule = await getProducerSchedule()
   if (!schedule) return
+  
+  _.each(producersTracking, async (producerTracking) {
+    await checkProducer(producerTracking, state, producers, schedule);
+  });
 
-  // Get the target producer from the active producers
-  const producer = _.find(producers, { owner: producer_account })
-  if (!producer) {
-    logger.debug({ producer_account }, `producer not an active producer`)
-    return
-  }
-
-  // Get the target producer within the schedule
-  const current_schedule = _.find(schedule.active.producers, { producer_name: producer_account })
-  if (!current_schedule) {
-    logger.debug({ producer_account }, `producer not in schedule`)
-    return
-  }
-
-  // Get the current signing key from the active schedule
-  const current_signing_key = current_schedule.block_signing_key;
-
-  // Ensure the current key in use is not a potential key to failover to
-  if (producer_signing_pubkeys.includes(current_signing_key)) {
-    logger.debug({ producer_signing_pubkeys, current_signing_key }, "removing current signing key from potential failover keys")
-    producer_signing_pubkeys = _.filter(producer_signing_pubkeys, (k) => k !== current_signing_key);
-  }
-
-  // If a key change is in progress, do not proceed
-  if (
-    incomingSigningKeyChange(current_signing_key, schedule, 'proposed')
-    || incomingSigningKeyChange(current_signing_key, schedule, 'pending')
-  ) {
-    // reset the last paid value to reinitiaize and prevent secondary trigger under certain circumstances
-    last_unpaid = -1
-    slack.send(`🕒 signing key changes pending in schedule`)
-    return
-  }
-
-  let { unpaid_blocks } = producer
-  logger.debug({ unpaid_blocks, last_unpaid }, "unpaid block states")
-
-  if (last_unpaid === -1) {
-    // If this is the first run (-1), just initialize and don't proceed
-    logger.info({ unpaid_blocks }, "initializing unpaid block count on first call")
-    last_unpaid = unpaid_blocks
-    return
-  } else if (unpaid_blocks < last_unpaid) {
-    // If the new unpaid is less than the old, the BP has claimed and this value needs to be reset
-    logger.debug({ last_unpaid, unpaid_blocks }, "resetting unpaid blocks, encountered reward claim")
-    last_unpaid = unpaid_blocks
-    return
-  } else if (unpaid_blocks > last_unpaid) {
-    // If the unpaid count is higher than the old count, new blocks were produced
-    const newBlocks = unpaid_blocks - last_unpaid
-    logger.info({ last_unpaid, unpaid_blocks }, `round success, witnessed ${newBlocks} new unpaid blocks`)
-    // Update the last unpaid value
-    last_unpaid = unpaid_blocks
-    // If any previously recorded rounds were recorded, reset them
-    if (rounds_missed > 0) {
-      const msg = `producer recovered after ${rounds_missed} rounds, witnessed ${newBlocks} new unpaid blocks`
-      logger.info({ rounds_missed }, msg)
-      slack.send(msg)
-      rounds_missed = 0
-    }
-  } else if (unpaid_blocks === last_unpaid) {
-    // If the unpaid blocks is the same as the last time - no new blocks have been produced
-    rounds_missed += 1
-    logger.info({ last_unpaid, unpaid_blocks, rounds_missed }, `producer missed a round!`)
-    slack.send(`⚠️ producer missed a round ${rounds_missed}/${rounds_missed_threshold}`)
-  }
-
+  const rounds_missed = _.reduce(producersTracking, (sum, {rounds_missed}) => sum + rounds_missed, 0)
   // If the new missed rounds exceeds the threshold, begin taking action
   if (rounds_missed >= rounds_missed_threshold) {
-    logger.debug({ rounds_missed, rounds_missed_threshold }, 'producer has exceeded missed round threshold, executing failover')
-    slack.send(`⚠️ producer exceeded missed round threshold ${rounds_missed}/${rounds_missed_threshold}`)
+    logger.debug({ rounds_missed, rounds_missed_threshold }, 'producers have exceeded missed round threshold, executing failover')
+    slack.send(`⚠️ producers exceeded missed round threshold ${rounds_missed}/${rounds_missed_threshold}`)
     // If keys exist, failover to one of them
-    if (producer_signing_pubkeys.length) {
-      const next_signing_key = producer_signing_pubkeys.shift()
-      const txid = await failover(next_signing_key)
-      logger.info({ txid, next_signing_key }, 'regproducer submitted to failover to next available node')
-      slack.send(`⚠️ regproducer submitted with new signing key of ${next_signing_key}, ${producer_signing_pubkeys} keys remaining in rotation (${txid})`)
+    if (producer_signing_pubkeys_nodes.length) {
+      const producer_signing_pubkeys = producer_signing_pubkeys_nodes.shift()
+      _.each(producer_signing_pubkeys, async (next_signing_key, i) {
+        const producerTracking = producersTracking[i];
+        const txid = await failover(producerTracking.producer_account, next_signing_key)
+        logger.info({ txid, producer_account: producerTracking.producer_account, next_signing_key }, 'regproducer submitted to failover to next available node')
+        slack.send(`⚠️ regproducer submitted with new signing key for ${producerTracking.producer_account} with ${next_signing_key}, ${producer_signing_pubkeys_nodes} keys remaining in rotation (${txid})`)
+      })
     } else {
-      // If no public keys exist, unregister the BP
-      const txid = await unregister()
-      logger.info({ txid }, 'unregprod submitted')
-      slack.send(`⚠️ producer has no backup nodes available, unregprod submitted (${txid})`)
+      // If no public keys exist, crash the app and attempt a reload of public keys in order to start over.
+      logger.info({ txid }, 'no keys, crashing app')
+      slack.send(`⚠️ producers have no backup keys available, crashing the app to try over`)
+      ensureExit();
     }
   }
 }
